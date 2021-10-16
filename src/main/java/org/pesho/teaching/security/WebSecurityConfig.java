fbe4fa422333acc9dc6da671eb1ad83b5d1fdfe8
Petar Petrov <pppepito86@gmail.com>
2021-10-16 23:23:45 +0300

    basic security

---
+++ src/main/java/org/pesho/teaching/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package org.pesho.teaching.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/admin").authenticated()
+				.anyRequest().permitAll()
+				.and()
+			.httpBasic();
+	}
+	
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+            throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("{noop}admin1")
+            .roles("ADMIN");
+    }
+
+}
